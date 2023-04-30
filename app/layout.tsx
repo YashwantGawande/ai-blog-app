83fbf61c47c8073b893aec1489bd0ba4fb82314b
YashwantGawande <gawandeyashwant7@gmail.com>
2023-04-30 23:56:16 +0530

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
-import './globals.css'
-import { Open_Sans } from 'next/font/google'
+import Navbar from "./(shared)/Navbar";
+import "./globals.css";
+import { Open_Sans } from "next/font/google";
 
-const openSans = Open_Sans({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog AI App',
-  description: 'Blog built in Next js that uses AI',
-}
+  title: "Blog AI App",
+  description: "Blog built in Next js that uses AI",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html className={openSans.className}lang="en">
-      <body >{children}</body>
+    <html className={openSans.className} lang="en">
+      <body>
+        <Navbar />
+        {children}
+        {/* <Footer/> */}
+      </body>
     </html>
-  )
+  );
 }
