3e0a470660037bf337dfc10ebacbd1591b5de585
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-01 23:43:15 +0530

    Add Trending, Tech, Travel, Other, Subscribe, Sidebar to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import Tech from "./(home)/Tech";
+import Travel from "./(home)/Travel";
+import Trending from "./(home)/Trending";
+import Other from "./(shared)/Other";
+import Sidebar from "./(shared)/Sidebar";
+import Subscribe from "./(shared)/Subscribe";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <main className="px-10 leading-7">
+      <Trending />
+      <div className="gap-10 mb-5 md:flex">
+        <div className="basis-3/4">
+          <Tech />
+          <Travel />
+          <Other />
+          <div className="hidden md:block">
+            <Subscribe />
+          </div>
+        </div>
+        <div className="basis-1/4">
+          <Sidebar />
+        </div>
+      </div>
+    </main>
+  );
 }
