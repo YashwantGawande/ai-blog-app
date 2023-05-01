92d93ff95ad7a0ac8c3c419525db2e2ccc757fa9
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-01 23:46:00 +0530

    Add Image with next/image

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import SocialLinks from "./SocialLinks";
 import Link from "next/link";
 import Image from "next/image";
+import Ad1 from "/public/assets/ad-1.jpg";
 
 type Props = {};
 
@@ -28,7 +29,20 @@ const Navbar = (props: Props) => {
             Blog dedicated towards AI and generation and job automation
           </p>
         </div>
-        <div className="relative w-auto h-32 basis-full bg-wh-500"></div>
+        <div className="relative w-auto h-32 basis-full bg-wh-500">
+          {" "}
+          <Image
+            fill
+            alt="advert-1"
+            placeholder="blur"
+            src={Ad1}
+            sizes="(max-width: 480px) 100vw,
+                (max-width: 768px) 75vw,
+                (max-width: 1060px) 50vw,
+                33vw"
+            style={{ objectFit: "cover" }}
+          />
+        </div>
       </div>
       <hr className="mx-10 border-1" />
     </header>
---
+++ app/(shared)/Other.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "./Card";
+
+type Props = {};
+
+const Other = (props: Props) => {
+  return (
+    <section className="pt-4 mb-16">
+      <hr className="border-1" />
+      {/* HEADER */}
+      <p className="my-8 text-2xl font-bold">Other Trending Posts</p>
+      <div className="grid-cols-2 gap-16 sm:grid">
+        <Card
+          className="mt-5 sm:mt-0"
+          imageHeight="h-80"
+          // post={otherPosts[0]}
+        />
+        <Card
+          className="mt-5 sm:mt-0"
+          imageHeight="h-80"
+          // post={otherPosts[1]}
+        />
+        <Card
+          className="mt-5 sm:mt-0"
+          imageHeight="h-80"
+          // post={otherPosts[2]}
+        />
+        <Card
+          className="mt-5 sm:mt-0"
+          imageHeight="h-80"
+          // post={otherPosts[3]}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Other;
