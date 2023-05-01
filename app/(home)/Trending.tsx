dea89a185dac6cfdba1e6360de8daee143eb0bc4
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-01 23:45:08 +0530

    Create Trending components

---
+++ app/(home)/Trending.tsx
@@ -0,0 +1,97 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type TrendingCardProps = {
+  className?: string;
+};
+
+const TrendingCard = ({ className }: TrendingCardProps) => {
+  return (
+    <Link
+      className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
+      // href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+      href="/"
+    >
+      <div className="relative z-0 w-full h-full">
+        <Image
+          fill
+          alt="tech"
+          // placeholder="blur"
+          src=""
+          // src={post?.image}
+          sizes="(max-width: 480px) 100vw,
+            (max-width: 768px) 75vw,
+            (max-width: 1060px) 50vw,
+            33vw"
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full z-1 bg-gradient-gradual" />
+      <div className="absolute bottom-0 left-0 p-3 z-2">
+        <h4 className="inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900">
+          {/* {post?.category} */}
+        </h4>
+        <div className="mt-2 text-wh-100">{/* {post?.title} */}</div>
+      </div>
+    </Link>
+  );
+};
+
+type Props = {};
+
+const Trending = (props: Props) => {
+  return (
+    <section className="pt-3 pb-10">
+      <div className="flex items-center gap-3">
+        <div className="px-8 py-2 text-sm font-bold bg-wh-900 text-wh-10">
+          TRENDING
+        </div>
+        <p className="text-sm">
+          Nunc enim lobortis quam risus et feugiat nibh eu ornare. Molestie sit
+          nulla dolor diam turpis.
+        </p>
+      </div>
+
+      {/* flex option */}
+      {/* <div className="flex justify-between gap-3 my-3">
+        <div className="basis-1/2 bg-wh-500 h-96"></div>
+        <div className="flex flex-col gap-3 basis-1/2 h-96">
+          <div className="basis-1/2 bg-wh-500"></div>
+          <div className="flex gap-3 basis-1/2">
+            <div className="basis-1/2 bg-wh-500"></div>
+            <div className="basis-1/2 bg-wh-500"></div>
+          </div>
+        </div>
+      </div> */}
+
+      {/* grid option */}
+      <div className="sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px] my-3">
+        <TrendingCard
+          className="col-span-2 row-span-2 bg-wh-500"
+          // post={trendingPosts[0]}
+        />
+        <TrendingCard
+          className="col-span-2 row-span-1 bg-wh-500"
+          // post={trendingPosts[1]}
+        />
+        <TrendingCard
+          className="col-span-1 row-span-1 bg-wh-500"
+          // post={trendingPosts[2]}
+        />
+        <TrendingCard
+          className="col-span-1 row-span-1 bg-wh-500"
+          // post={trendingPosts[3]}
+        />
+      </div>
+
+      <p className="text-sm">
+        Id cursus purus adipiscing ipsum pretium. Scelerisque suspendisse
+        pharetra ultrices mauris ut lacus sagittis pharetra dictum. Congue
+        viverra in aliquam feugiat pellentesque.
+      </p>
+    </section>
+  );
+};
+
+export default Trending;
