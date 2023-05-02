dcc1439c505f9dbd864d76f79ed69ea6b68f3800
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-02 23:49:31 +0530

    Update trending component to get data from database

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -1,25 +1,27 @@
+import { Post } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 type TrendingCardProps = {
   className?: string;
+  post: Post;
 };
 
-const TrendingCard = ({ className }: TrendingCardProps) => {
+const TrendingCard = ({ className,post }: TrendingCardProps) => {
   return (
     <Link
       className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
-      // href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
-      href="/"
+      href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+      
     >
       <div className="relative z-0 w-full h-full">
         <Image
           fill
           alt="tech"
-          // placeholder="blur"
-          src=""
-          // src={post?.image}
+          blurDataURL="data:..."
+          placeholder="blur"
+          src={post?.image}
           sizes="(max-width: 480px) 100vw,
             (max-width: 768px) 75vw,
             (max-width: 1060px) 50vw,
@@ -30,17 +32,19 @@ const TrendingCard = ({ className }: TrendingCardProps) => {
       <div className="absolute top-0 left-0 w-full h-full z-1 bg-gradient-gradual" />
       <div className="absolute bottom-0 left-0 p-3 z-2">
         <h4 className="inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900">
-          {/* {post?.category} */}
+          {post?.category}
         </h4>
-        <div className="mt-2 text-wh-100">{/* {post?.title} */}</div>
+        <div className="mt-2 text-wh-100">{post?.title}</div>
       </div>
     </Link>
   );
 };
 
-type Props = {};
+type Props = {
+  trendingPosts: Array<Post>;
+};
 
-const Trending = (props: Props) => {
+const Trending = ({ trendingPosts }: Props) => {
   return (
     <section className="pt-3 pb-10">
       <div className="flex items-center gap-3">
@@ -69,19 +73,19 @@ const Trending = (props: Props) => {
       <div className="sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px] my-3">
         <TrendingCard
           className="col-span-2 row-span-2 bg-wh-500"
-          // post={trendingPosts[0]}
+          post={trendingPosts[0]}
         />
         <TrendingCard
           className="col-span-2 row-span-1 bg-wh-500"
-          // post={trendingPosts[1]}
+          post={trendingPosts[1]}
         />
         <TrendingCard
           className="col-span-1 row-span-1 bg-wh-500"
-          // post={trendingPosts[2]}
+          post={trendingPosts[2]}
         />
         <TrendingCard
           className="col-span-1 row-span-1 bg-wh-500"
-          // post={trendingPosts[3]}
+          post={trendingPosts[3]}
         />
       </div>
 
