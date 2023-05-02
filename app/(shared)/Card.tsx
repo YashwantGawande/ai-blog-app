7d039c85dfc9052dc4d0cf3e094b6d9da230f3a1
YashwantGawande <gawandeyashwant7@gmail.com>
2023-05-02 23:45:17 +0530

    Update card to show data from database

--- app/(shared)/Card.tsx
+++ app/(shared)/Card.tsx
@@ -1,3 +1,4 @@
+import { Post } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -7,6 +8,7 @@ type Props = {
   imageHeight?: string;
   isSmallCard?: boolean;
   isLongForm?: boolean;
+  post: Post;
 };
 
 const Card = ({
@@ -14,21 +16,27 @@ const Card = ({
   imageHeight,
   isSmallCard = false,
   isLongForm = false,
+  post,
 }: Props) => {
+  const { id, title, createdAt, updatedAt, image, snippet, author } =
+    post || {};
+
+  const date = new Date(createdAt);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", options as any);
+
   return (
     <div className={className}>
       <Link
         className="basis-full hover:opacity-70"
-        // href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
-        href=""
+        href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
       >
         <div className={`relative w-auto mb-3 ${imageHeight}`}>
           <Image
             fill
             alt="tech"
-            // placeholder="blur"
-            // src={image}
-            src=""
+            placeholder="blur"
+            src={image}
             sizes="(max-width: 480px) 100vw,
               (max-width: 768px) 75vw,
               (max-width: 1060px) 50vw,
@@ -38,30 +46,27 @@ const Card = ({
         </div>
       </Link>
       <div className="basis-full">
-        <Link
-        // href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
-        href=""
-        >
+        <Link href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}>
           <h4
             className={`font-bold hover:text-accent-green
         ${isSmallCard ? "text-base" : "text-lg"}
         ${isSmallCard ? "line-clamp-2" : ""}
       `}
           >
-            {/* {title} */}
+            {title}
           </h4>
         </Link>
 
         <div className={`${isSmallCard ? "my-2" : "flex my-3"} gap-3`}>
-          <h5 className="text-xs font-semibold">{/* {author} */}</h5>
-          <h6 className="text-xs text-wh-300">{/* {formattedDate} */}</h6>
+          <h5 className="text-xs font-semibold">{author}</h5>
+          <h6 className="text-xs text-wh-300">{formattedDate}</h6>
         </div>
         <p
           className={`text-wh-500 ${
             isLongForm ? "line-clamp-5" : "line-clamp-3"
           }`}
         >
-          {/* {snippet} */}
+          {snippet}
         </p>
       </div>
     </div>
